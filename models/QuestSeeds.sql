dfb1346632ac0b3c22da73d536356501b064827b
Missi3223 <44407731+Missi3223@users.noreply.github.com>
2019-03-27 17:01:17 -0600

    Cleaning up the files

--- models/QuestSeeds.sql
+++ models/QuestSeeds.sql
@@ -4,23 +4,23 @@ INSERT INTO quests (questName, questDescribe, Location, completed, rewardDescrib
 VALUES ("Snooze an A.M. Eatery", "Start your day off with an egg dish", "Fort Collins", false, "15% off total");
 INSERT INTO quests (questName, questDescribe, Location, completed, rewardDescribe)
 VALUES ("CopperMuse Distillery", "Take a tour" , "Fort Collins", false, "Free Beer"),
-
-("Beau Jo's" , "Buy any large pizza", "Fort Collins" , false, "Free cheese bread"),
-
-("Curiosities", "Spend $10", "Fort Collins", false, "10% off"),
-
-("Nature's Own", "Spend $20", "Fort Collins", false, "10% off"),
-
-("Town Pump" , "Grab some cherries", "Fort Collins" ,false, "25% off"),
-
-("Original Pizza", "Stop in a grab a Large pizza" , "Broomfield", false , "Free slice of cheese pizza!"),
-
-("At Home", "Spend $25" , "Broomfield", false,"25% off a Single Item!" ),
-
-("Chipper's Lane" , "Buy Two Pitchers of beer", "Broomfield", false, "free shoe rental"),
-
-("Night Owl Lounge", "Buy a pitcher of beer, and play a round of pool", "Broomfield", false, "free specialty drink"),
-
-("Infinitus Pizza PIE", "Buy a medium pizza", "Broomfield", false, "Free 2 liter soda"),
-
-("Shell" , "Purchase 1 tank of gas", "Broomfield", false, "Free car wash");
+INSERT INTO quests (questName, questDescribe, Location, completed, rewardDescribe)
+VALUES ("Beau Jo's" , "Buy any large pizza", "Fort Collins" , false, "Free cheese bread"),
+INSERT INTO quests (questName, questDescribe, Location, completed, rewardDescribe)
+VALUE S("Curiosities", "Spend $10", "Fort Collins", false, "10% off"),
+INSERT INTO quests (questName, questDescribe, Location, completed, rewardDescribe)
+VALUES ("Nature's Own", "Spend $20", "Fort Collins", false, "10% off"),
+INSERT INTO quests (questName, questDescribe, Location, completed, rewardDescribe)
+VALUES ("Town Pump" , "Grab some cherries", "Fort Collins" ,false, "25% off"),
+INSERT INTO quests (questName, questDescribe, Location, completed, rewardDescribe)
+VALUES ("Original Pizza", "Stop in a grab a Large pizza" , "Broomfield", false , "Free slice of cheese pizza!"),
+INSERT INTO quests (questName, questDescribe, Location, completed, rewardDescribe)
+VALUES ("At Home", "Spend $25" , "Broomfield", false,"25% off a Single Item!" ),
+INSERT INTO quests (questName, questDescribe, Location, completed, rewardDescribe)
+VALUES ("Chipper's Lane" , "Buy Two Pitchers of beer", "Broomfield", false, "free shoe rental"),
+INSERT INTO quests (questName, questDescribe, Location, completed, rewardDescribe)
+VALUES ("Night Owl Lounge", "Buy a pitcher of beer, and play a round of pool", "Broomfield", false, "free specialty drink"),
+INSERT INTO quests (questName, questDescribe, Location, completed, rewardDescribe)
+VALUES ("Infinitus Pizza PIE", "Buy a medium pizza", "Broomfield", false, "Free 2 liter soda"),
+INSERT INTO quests (questName, questDescribe, Location, completed, rewardDescribe)
+VALUES ("Shell" , "Purchase 1 tank of gas", "Broomfield", false, "Free car wash");
